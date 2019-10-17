ab13767e8d6e1a10ba08266bba479b1c4bef4545
Christopher Lamm <chris@theantichris.com>
2019-10-17 15:03:40 -0400

    feat: echoing path

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
+// EchoServer is an "echo" server.
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", handler)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
