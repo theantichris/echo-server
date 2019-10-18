0e8c162d060b93ce9c89d3bf3ced6026876eedf6
Christopher Lamm <chris@theantichris.com>
2019-10-18 07:35:24 -0400

    feat: counting number of requests handled

--- main.go
+++ main.go
@@ -6,19 +6,37 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 )
+
+var mu sync.Mutex
+var count int
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
 
 	port := getPort()
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
+// handler echoes the Path component of the requested URL.
 func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
+// counter echos the number of calls so far.
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+// getPort gets the port specified in the env or defaults to 5000.
 func getPort() string {
 	port := os.Getenv("PORT")
 	if port == "" {
