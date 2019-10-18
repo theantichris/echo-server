68fb20d4386ee93359a665d5920af4a35764ea8d
Christopher Lamm <chris@theantichris.com>
2019-10-18 07:47:02 -0400

    feat: removed count, echoing request

--- main.go
+++ main.go
@@ -14,26 +14,29 @@ var count int
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/count", counter)
 
 	port := getPort()
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
-// handler echoes the Path component of the requested URL.
+// handler echoes the HTTP request.
 func handler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	count++
-	mu.Unlock()
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
 
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-}
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
 
-// counter echos the number of calls so far.
-func counter(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	fmt.Fprintf(w, "Count %d\n", count)
-	mu.Unlock()
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
 
 // getPort gets the port specified in the env or defaults to 5000.
